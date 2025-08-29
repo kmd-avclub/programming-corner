3bd26625e6f1cb67beb74112e973ed5c4587a737
kmd-avclub <65889941+kmd-avclub@users.noreply.github.com>
2025-08-29 09:28:05 +0200

    Update potTo3LED.ino

--- arduino-sketches/potTo3LED/potTo3LED.ino
+++ arduino-sketches/potTo3LED/potTo3LED.ino
@@ -1,37 +1,47 @@
-int analogValue; //set a storage place for our analogRead(0)
-const int analogPin=0; //we declare our pins
-const int ledG=3;
-const int ledY=4;
-const int ledR=5;
-const int dt=10;  //delay time
+// Pin definitions
+const int ANALOG_INPUT_PIN = 0;
+const int LED_GREEN_PIN = 3;
+const int LED_YELLOW_PIN = 4;
+const int LED_RED_PIN = 5;
+
+// ADC constants
+const int ADC_MAX_VALUE = 1023;
+const int ADC_MIN_THRESHOLD = 10;
+
+// Calculate thresholds automatically
+const int ADC_LOW_THRESHOLD = ADC_MAX_VALUE / 3;      // 341
+const int ADC_MID_THRESHOLD = (ADC_MAX_VALUE * 2) / 3; // 682
+
+const int LOOP_DELAY_MS = 10;
+
+int analogValue;
 
 void setup() {
-pinMode(ledG,OUTPUT);
-pinMode(ledY,OUTPUT);
-pinMode(ledR,OUTPUT);
+  //declare pins
+  pinMode(LED_GREEN_PIN, OUTPUT);
+  pinMode(LED_YELLOW_PIN, OUTPUT);
+  pinMode(LED_RED_PIN, OUTPUT);
+  //turn LEDs off
+  digitalWrite(LED_GREEN_PIN, LOW);
+  digitalWrite(LED_YELLOW_PIN, LOW);
+  digitalWrite(LED_RED_PIN, LOW);
 }
-void loop() {
-analogValue=analogRead(analogPin);
 
-if (analogValue > 10 && analogValue < 341){
-  digitalWrite(ledG,HIGH);
-  digitalWrite(ledY,LOW);
-  digitalWrite(ledR,LOW);
-}
-else if (analogValue > 341 && analogValue < 682){
-  digitalWrite(ledG,HIGH);
-  digitalWrite(ledY,HIGH);
-  digitalWrite(ledR,LOW);
-}
-else if (analogValue > 682){
-  digitalWrite(ledG,HIGH);
-  digitalWrite(ledY,HIGH);
-  digitalWrite(ledR,HIGH);s
-}
-else if (analogValue < 10){
-  digitalWrite(ledG,LOW);
-  digitalWrite(ledY,LOW);
-  digitalWrite(ledR,LOW);
-}
-delay(dt);
+void loop() {
+  analogValue = analogRead(ANALOG_INPUT_PIN);
+  
+  // Turn on LEDs based on level
+  if (analogValue >= ADC_MIN_THRESHOLD) {
+    digitalWrite(LED_GREEN_PIN, HIGH);  // Always first
+    
+    if (analogValue > ADC_LOW_THRESHOLD) {
+      digitalWrite(LED_YELLOW_PIN, HIGH);  // Medium level
+      
+      if (analogValue > ADC_MID_THRESHOLD) {
+        digitalWrite(LED_RED_PIN, HIGH);  // High level
+      }
+    }
+  }
+  
+  delay(LOOP_DELAY_MS);
 }
